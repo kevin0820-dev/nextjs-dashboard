0745ecbbe83333f469a7937d44035dd990757d99
kevin0820-dev <kevinallen5877066@gmail.com>
2024-08-03 18:36:40 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className={styles.shape}>
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
